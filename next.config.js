e988018c0af8e2f03089d31acba7e6b021a229a3
Kiran <kiranmavi5502@gmail.com>
2025-06-19 23:28:10 +0100

    tailwind css working in build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  distDir: 'out',
+  trailingSlash: true,
   // basePath: '/Medicare',
   // assetPrefix: '/Medicare/',
 };
